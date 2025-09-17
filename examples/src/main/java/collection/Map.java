4889c70117569379e18bfbba3360f3635dac40be
Kim-sangjae <kimsangjae94114@gmail.com>
2025-09-17 18:09:23 +0900

    Map example upload

---
+++ examples/src/main/java/collection/Map.java
@@ -0,0 +1,18 @@
+package collection;
+
+public class Map {
+    public static void main(String[] args) {
+
+//
+//        학생 ID 입력('exit' 입력 시 종료): hello
+//        ID가 추가 되었습니다.
+//                학생 ID 입력('exit' 입력 시 종료): java
+//        ID가 추가 되었습니다.
+//                학생 ID 입력('exit' 입력 시 종료): programmer
+//        ID가 추가 되었습니다.
+//                학생 ID 입력('exit' 입력 시 종료): java
+//        이미 등록 된 ID입니다.
+//        학생 ID 입력('exit' 입력 시 종료): exit
+//        모든 학생의 ID : [java, programmer, hello]
+    }
+}
