3d0d1f2d3500b1bde37d4921016a3d184890656d
Kim-sangjae <kimsangjae94114@gmail.com>
2025-09-17 18:12:11 +0900

    Map example upload

--- examples/src/main/java/collection/Application4.java
+++ examples/src/main/java/collection/Application4.java
@@ -5,76 +5,33 @@ import java.util.Map;
 
 public class Application4 {
     public static void main(String[] args) {
-        //이름을 입력하면 전화번호를 검색할 수 있도록 전화번호부를 구현하세요.
-//        이름과 전화번호를 띄어쓰기 기준으로 입력하세요(검색하려면 'search', 종료하려면 'exit'):홍길동010 - 1234 - 5678
-//        입력이 잘못 되었습니다.다음 양식으로 입력해주세요: <이름 > < 전화번호 >
-//        이름과 전화번호를 띄어쓰기 기준으로 입력하세요 (검색하려면 'search', 종료하려면 'exit'):홍길동 010 - 1234 - 5678
-//        추가 완료 :홍길동 010 - 1234 - 5678
-//        이름과 전화번호를 띄어쓰기 기준으로 입력하세요(검색하려면 'search', 종료하려면 'exit'):유관순 010 - 9876 - 5432
-//        추가 완료 :유관순 010 - 9876 - 5432
-//        이름과 전화번호를 띄어쓰기 기준으로 입력하세요(검색하려면 'search', 종료하려면 'exit'):search
-//        검색 할 이름:
-//        유관순
-//        유관순씨의 전화번호 :010 - 9876 - 5432
-//        이름과 전화번호를 띄어쓰기 기준으로 입력하세요(검색하려면 'search', 종료하려면 'exit'):search
-//        검색 할 이름:
-//        이순신
-//        이순신씨의 번호는 등록 되어 있지 않습니다.
-//        이름과 전화번호를 띄어쓰기 기준으로 입력하세요(검색하려면 'search', 종료하려면 'exit'):exit
+        Set<String> studentIDs = new HashSet<>();
 
-        Scanner sc = new Scanner(System.in);
+        try (Scanner scanner = new Scanner(System.in)) {
+            while (true) {
+                System.out.print("학생 ID 입력('exit' 입력 시 종료): ");
+                String id = scanner.nextLine().trim();
 
-        try {
-            boolean check = true;
-            HashMap<String, String> hashInfo = new HashMap<>();
-            while (check) {
-                System.out.println("이름과 전화번호를 띄어쓰기 기준으로 입력하세요 (검색하려면 'search', 종료하려면 'exit') : ");
-                String cusInfo = sc.nextLine();
-                String[] info = cusInfo.split(" ");
-                if (cusInfo.equals("exit")){
+                if(id.equals("exit")) {
                     break;
                 }
 
-                if (cusInfo.equals("search")) {
-                    System.out.print("검색할 이름 : ");
-                    String searchKey = sc.nextLine().trim();
-
-                    if (hashInfo.containsKey(searchKey)) {
-                        System.out.println(searchKey + "씨의 전화번호 : " + hashInfo.get(searchKey));
-                    } else {
-                        System.out.println(searchKey + "씨의 번호는 등록되어 있지 않습니다.");
-                    }
-                    continue; // 다음 입력 대기
+                if(id.isEmpty()) {
+                    System.out.println("유효한 ID를 입력하세요.");
+                    continue;
                 }
 
-
-                if (cusInfo.isEmpty() || info.length != 2) {
-                    System.out.println("입력이 잘못 되었습니다. 다음 양식으로 입력해주세요 : <이름> <전화번호>");
-                    continue;
+                if(!studentIDs.add(id)) {
+                    System.out.println("이미 등록 된 ID입니다.");
                 } else {
-
-                    hashInfo.put(info[0], info[1]);
-                    Iterator<String> keyIter = hashInfo.keySet().iterator();
-
-                    while(keyIter.hasNext()) {
-
-                        String key = (String)keyIter.next();
-
-                        if(key.equals(info[0])){
-
-                            System.out.println("추가 완료 : " + key + " " + hashInfo.get(key));
-                        }
-                    }
-
-
-
+                    System.out.println("ID가 추가 되었습니다.");
                 }
             }
-        } catch (Exception e) {
-            e.printStackTrace();
         }
 
-
+        System.out.println("모든 학생의 ID : " + studentIDs);
     }//main
-}
+
+    }
+
 
--- examples/src/main/java/collection/Map.java
+++ examples/src/main/java/collection/Map.java
@@ -1,18 +1,81 @@
 package collection;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Scanner;
+
 public class Map {
     public static void main(String[] args) {
+        //이름을 입력하면 전화번호를 검색할 수 있도록 전화번호부를 구현하세요.
+//        이름과 전화번호를 띄어쓰기 기준으로 입력하세요(검색하려면 'search', 종료하려면 'exit'):홍길동010 - 1234 - 5678
+//        입력이 잘못 되었습니다.다음 양식으로 입력해주세요: <이름 > < 전화번호 >
+//        이름과 전화번호를 띄어쓰기 기준으로 입력하세요 (검색하려면 'search', 종료하려면 'exit'):홍길동 010 - 1234 - 5678
+//        추가 완료 :홍길동 010 - 1234 - 5678
+//        이름과 전화번호를 띄어쓰기 기준으로 입력하세요(검색하려면 'search', 종료하려면 'exit'):유관순 010 - 9876 - 5432
+//        추가 완료 :유관순 010 - 9876 - 5432
+//        이름과 전화번호를 띄어쓰기 기준으로 입력하세요(검색하려면 'search', 종료하려면 'exit'):search
+//        검색 할 이름:
+//        유관순
+//        유관순씨의 전화번호 :010 - 9876 - 5432
+//        이름과 전화번호를 띄어쓰기 기준으로 입력하세요(검색하려면 'search', 종료하려면 'exit'):search
+//        검색 할 이름:
+//        이순신
+//        이순신씨의 번호는 등록 되어 있지 않습니다.
+//        이름과 전화번호를 띄어쓰기 기준으로 입력하세요(검색하려면 'search', 종료하려면 'exit'):exit
 
-//
-//        학생 ID 입력('exit' 입력 시 종료): hello
-//        ID가 추가 되었습니다.
-//                학생 ID 입력('exit' 입력 시 종료): java
-//        ID가 추가 되었습니다.
-//                학생 ID 입력('exit' 입력 시 종료): programmer
-//        ID가 추가 되었습니다.
-//                학생 ID 입력('exit' 입력 시 종료): java
-//        이미 등록 된 ID입니다.
-//        학생 ID 입력('exit' 입력 시 종료): exit
-//        모든 학생의 ID : [java, programmer, hello]
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            boolean check = true;
+            HashMap<String, String> hashInfo = new HashMap<>();
+            while (check) {
+                System.out.println("이름과 전화번호를 띄어쓰기 기준으로 입력하세요 (검색하려면 'search', 종료하려면 'exit') : ");
+                String cusInfo = sc.nextLine();
+                String[] info = cusInfo.split(" ");
+                if (cusInfo.equals("exit")){
+                    break;
+                }
+
+                if (cusInfo.equals("search")) {
+                    System.out.print("검색할 이름 : ");
+                    String searchKey = sc.nextLine().trim();
+
+                    if (hashInfo.containsKey(searchKey)) {
+                        System.out.println(searchKey + "씨의 전화번호 : " + hashInfo.get(searchKey));
+                    } else {
+                        System.out.println(searchKey + "씨의 번호는 등록되어 있지 않습니다.");
+                    }
+                    continue; // 다음 입력 대기
+                }
+
+
+                if (cusInfo.isEmpty() || info.length != 2) {
+                    System.out.println("입력이 잘못 되었습니다. 다음 양식으로 입력해주세요 : <이름> <전화번호>");
+                    continue;
+                } else {
+
+                    hashInfo.put(info[0], info[1]);
+                    Iterator<String> keyIter = hashInfo.keySet().iterator();
+
+                    while(keyIter.hasNext()) {
+
+                        String key = (String)keyIter.next();
+
+                        if(key.equals(info[0])){
+
+                            System.out.println("추가 완료 : " + key + " " + hashInfo.get(key));
+                        }
+                    }
+
+
+
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+
     }
 }
